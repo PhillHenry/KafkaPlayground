2dc38b3436bafea909294545860a2ff1ab9bf1c5
PhillipHenry <PhillHenry@gmail.com>
2023-06-29 14:41:00 +0100

    Silly bug

--- modules/core/src/main/scala/uk/co/odinconsultants/kafka/KafkaDemoMain.scala
+++ modules/core/src/main/scala/uk/co/odinconsultants/kafka/KafkaDemoMain.scala
@@ -12,7 +12,11 @@ import uk.co.odinconsultants.dreadnought.docker.CatsDocker.interpret
 import uk.co.odinconsultants.dreadnought.docker.KafkaAntics.{createCustomTopic, produceMessages}
 import uk.co.odinconsultants.dreadnought.docker.Logging.verboseWaitFor
 import uk.co.odinconsultants.dreadnought.docker.PopularContainers.startKafkaOnPort
-import uk.co.odinconsultants.dreadnought.docker.SparkStructuredStreamingMain.{startSlave, startSparkCluster, waitForMaster}
+import uk.co.odinconsultants.dreadnought.docker.SparkStructuredStreamingMain.{
+  startSlave,
+  startSparkCluster,
+  waitForMaster,
+}
 import uk.co.odinconsultants.dreadnought.docker.ZKKafkaMain.{kafkaEcosystem, startKafkaCluster}
 
 import scala.concurrent.duration.*
@@ -22,38 +26,72 @@ object KafkaDemoMain extends IOApp.Simple {
   val TOPIC_NAME = "test_topic"
   val BOOTSTRAP  = "kafka_bootstrap"
   val BROKER     = "zk"
-  val kafkaPort  = port"9093"
+  val kafkaPort  = port"9092"
 
   /** TODO
     * Pull images
     */
   def run: IO[Unit] = for {
-    client       <- CatsDocker.client
-    (zk, kafka1) <- startKafkaCluster(client, verboseWaitFor, 20.seconds)
-    zkName       <- CatsDocker.interpret(client, Free.liftF(NamesRequest(zk)))
-    kafkaStart   <- Deferred[IO, String]
-    kafkaLatch    = verboseWaitFor("started (kafka.server.Kafka", kafkaStart)
-    kafka2       <- interpret(client, Free.liftF(startKafkaOnPort(kafkaPort, zkName)))
-    _            <- interpret(client, Free.liftF(
+    client     <- CatsDocker.client
+    kafkaStart <- Deferred[IO, String]
+    kafkaLatch  = verboseWaitFor("started (kafka.server.Kafka", kafkaStart)
+    kafka2     <- interpret(client, kafkaEcosystem(kafkaLatch, kafkaPort))
+    _          <- interpret(
+                    client,
+                    Free.liftF(
                       LoggingRequest(kafka2, kafkaLatch)
-                    ))
-    _            <- kafkaStart.get.timeout(20.seconds)
-    _            <- sendMessages
-    _            <- race(toInterpret(client))(
-                      List(kafka2, kafka1, zk).map(StopRequest.apply)
-                    )
+                    ),
+                  )
+    _          <- kafkaStart.get.timeout(20.seconds)
+//    _            <- sendMessages
+    _          <- race(toInterpret(client))(
+                    List(kafka2).map(StopRequest.apply)
+                  )
   } yield println("Started and stopped ZK and 2 kafka brokers")
 
+  def kafkaEcosystem(
+      kafkaLogging: String => IO[Unit],
+      hostPort:     Port,
+  ): Free[ManagerRequest, ContainerId] =
+    for {
+      kafka1 <- Free.liftF(startKafkaOnPort(hostPort))
+      _      <- Free.liftF(
+                  LoggingRequest(kafka1, kafkaLogging)
+                )
+    } yield kafka1
+
+  def startKafkaOnPort(
+      hostPort: Port
+  ): StartRequest = StartRequest(
+    ImageName("docker.io/bitnami/kafka:3.5"),
+    Command("/opt/bitnami/scripts/kafka/entrypoint.sh /run.sh"),
+    List(
+      "KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper:2181",
+      "ALLOW_PLAINTEXT_LISTENER=yes",
+      //      "KAFKA_INTER_BROKER_LISTENER_NAME=LISTENER_BOB",
+      //      "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=LISTENER_BOB:PLAINTEXT",
+      s"KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://127.0.0.1:${hostPort.value}",
+      "KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1",
+      "KAFKA_TRANSACTION_ABORT_TIMED_OUT_TRANSACTION_CLEANUP_INTERVAL_MS=60000",
+      "KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1",
+      "KAFKA_AUTHORIZER_CLASS_NAME=kafka.security.authorizer.AclAuthorizer",
+      "KAFKA_ALLOW_EVERYONE_IF_NO_ACL_FOUND=true",
+    ),
+    List(9092 -> hostPort.value),
+    List.empty,
+  )
+
   private val sendMessages: IO[Unit] = {
-    createCustomTopic(TOPIC_NAME)
-    val bootstrapServer = s"localhost:${kafkaPort.value}"
+    val bootstrapServer                                        = s"localhost:${kafkaPort.value}"
     val producerSettings: ProducerSettings[IO, String, String] =
       ProducerSettings[IO, String, String]
         .withBootstrapServers(bootstrapServer)
-    KafkaAntics.produce(producerSettings, TOPIC_NAME)
+    val messages                                               = KafkaAntics
+      .produce(producerSettings, TOPIC_NAME)
       .handleErrorWith(x => Stream.eval(IO(x.printStackTrace())))
       .compile
       .drain
+    IO(createCustomTopic(TOPIC_NAME)) *> messages
   }
 
 }
