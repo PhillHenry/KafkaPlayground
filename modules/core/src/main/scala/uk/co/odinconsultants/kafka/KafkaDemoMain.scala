60b4ee55cfda30dae1539e6ce62366308eb097cd
PhillipHenry <PhillHenry@gmail.com>
2023-06-30 10:32:16 +0100

    Doesn't close down all the containers but it does otherwise work

--- modules/core/src/main/scala/uk/co/odinconsultants/kafka/KafkaDemoMain.scala
+++ modules/core/src/main/scala/uk/co/odinconsultants/kafka/KafkaDemoMain.scala
@@ -19,6 +19,7 @@ import uk.co.odinconsultants.dreadnought.docker.SparkStructuredStreamingMain.{
 }
 import uk.co.odinconsultants.dreadnought.docker.ZKKafkaMain.{kafkaEcosystem, startKafkaCluster}
 
+import java.util.UUID
 import scala.concurrent.duration.*
 
 object KafkaDemoMain extends IOApp.Simple {
@@ -27,6 +28,7 @@ object KafkaDemoMain extends IOApp.Simple {
   val BOOTSTRAP  = "kafka_bootstrap"
   val BROKER     = "zk"
   val kafkaPort  = port"9092"
+  val clusterId  = UUID.randomUUID().toString
 
   /** TODO
     * Pull images
@@ -35,7 +37,7 @@ object KafkaDemoMain extends IOApp.Simple {
     client     <- CatsDocker.client
     kafkaStart <- Deferred[IO, String]
     kafkaLatch  = verboseWaitFor("started (kafka.server.Kafka", kafkaStart)
-    kafka2     <- interpret(client, kafkaEcosystem(kafkaLatch, kafkaPort))
+    kafka2     <- interpret(client, startKafkas(3, kafkaLatch))
     _          <- interpret(
                     client,
                     Free.liftF(
@@ -43,42 +45,66 @@ object KafkaDemoMain extends IOApp.Simple {
                     ),
                   )
     _          <- kafkaStart.get.timeout(20.seconds)
-//    _            <- sendMessages
+    _          <- IO(createCustomTopic(TOPIC_NAME))
+    _          <- sendMessages
     _          <- race(toInterpret(client))(
                     List(kafka2).map(StopRequest.apply)
                   )
   } yield println("Started and stopped ZK and 2 kafka brokers")
 
+  def startKafkas(numBrokers: Int, kafkaLogging: String => IO[Unit]): Free[ManagerRequest, ContainerId] = {
+    val meta   = for {
+      brokerId <- 1 to numBrokers
+      port     <- Port.fromInt(9091 + brokerId)
+    } yield (port, brokerId, s"kafka$brokerId")
+    val quorum = meta.map { case (port, brokerId, name) => s"$brokerId@$name:$port" }.mkString(",")
+    println(s"quorum = $quorum")
+    val frees: Seq[Free[ManagerRequest, ContainerId]] = meta.map { case (port, brokerId, name) =>
+      kafkaEcosystem(kafkaLogging, port, brokerId, quorum, name)
+    }
+    frees.tail.foldLeft(frees.head) { case (x, y) =>
+      x.flatMap(_ => y)
+    }
+  }
+
   def kafkaEcosystem(
       kafkaLogging: String => IO[Unit],
       hostPort:     Port,
+      brokerId:     Int,
+      quorum:       String,
+      name:         String,
   ): Free[ManagerRequest, ContainerId] =
     for {
-      kafka1 <- Free.liftF(startKafkaOnPort(hostPort))
+      kafka1 <- Free.liftF(startKafkaOnPort(hostPort, brokerId, quorum, name))
       _      <- Free.liftF(
                   LoggingRequest(kafka1, kafkaLogging)
                 )
     } yield kafka1
 
   def startKafkaOnPort(
-      hostPort: Port
+      hostPort: Port,
+      brokerId: Int,
+      quorum:   String,
+      name:     String,
   ): StartRequest = StartRequest(
     ImageName("docker.io/bitnami/kafka:3.5"),
     Command("/opt/bitnami/scripts/kafka/entrypoint.sh /run.sh"),
     List(
       "KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper:2181",
       "ALLOW_PLAINTEXT_LISTENER=yes",
-      //      "KAFKA_INTER_BROKER_LISTENER_NAME=LISTENER_BOB",
-      //      "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=LISTENER_BOB:PLAINTEXT",
       s"KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://127.0.0.1:${hostPort.value}",
       "KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1",
       "KAFKA_TRANSACTION_ABORT_TIMED_OUT_TRANSACTION_CLEANUP_INTERVAL_MS=60000",
       "KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1",
       "KAFKA_AUTHORIZER_CLASS_NAME=kafka.security.authorizer.AclAuthorizer",
       "KAFKA_ALLOW_EVERYONE_IF_NO_ACL_FOUND=true",
+      s"BROKER_ID=$brokerId",
+      s"CLUSTER_ID=$clusterId",
+      s"KAFKA_CONTROLLER_QUORUM_VOTERS=$quorum",
     ),
     List(9092 -> hostPort.value),
     List.empty,
+    Some(name),
   )
 
   private val sendMessages: IO[Unit] = {
@@ -91,7 +117,7 @@ object KafkaDemoMain extends IOApp.Simple {
       .handleErrorWith(x => Stream.eval(IO(x.printStackTrace())))
       .compile
       .drain
-    IO(createCustomTopic(TOPIC_NAME)) *> messages
+    messages
   }
 
 }
