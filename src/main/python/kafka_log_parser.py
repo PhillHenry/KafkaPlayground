c3a9cdd4c036aff347b51c735cb256d3b350a50a
PhillipHenry <PhillHenry@gmail.com>
2023-07-11 10:12:56 +0100

    Refactor

--- src/main/python/kafka_log_parser.py
+++ src/main/python/kafka_log_parser.py
@@ -10,3 +10,15 @@ class LogLine:
         self.timestamp_str = (elements[3] + " " + elements[4]).replace("[", "").replace("]", "")
         self.timestamp = datetime.strptime(self.timestamp_str, DATETIME_FORMAT)
 
+
+def parse_logs(filename: str) -> dict:
+    machine_to_logs = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            try:
+                log = LogLine(line)
+                logs = machine_to_logs.get(log.machine, [])
+                machine_to_logs[log.machine] = logs + [log]
+            except Exception:
+                print(f"Could not parse line:\n{line}")
+    return machine_to_logs
--- src/main/python/plot_log_activity.py
+++ src/main/python/plot_log_activity.py
@@ -1,27 +1,25 @@
 import sys
-from kafka_log_parser import LogLine, DATETIME_FORMAT
+from kafka_log_parser import LogLine, DATETIME_FORMAT, parse_logs
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-if __name__ == "__main__":
-    filename = sys.argv[1]
-    machine_to_logs = {}
-    with open(filename, 'r') as f:
-        for line in f:
-            try:
-                log = LogLine(line)
-                logs = machine_to_logs.get(log.machine, [])
-                machine_to_logs[log.machine] = logs + [log]
-            except Exception:
-                print(f"Could not parse line:\n{line}")
+
+def do_plot(filename: str):
+    machine_to_logs = parse_logs(filename)
     fig, ax = plt.subplots(1, 1)
     colours = ['red', 'blue', 'yellow']
     for i, machine in enumerate(machine_to_logs.keys()):
         logs = [l.timestamp for l in (machine_to_logs[machine])]
         ax.hist(logs, bins=100, color=colours[i])
     ax.xaxis.set_major_locator(mdates.SecondLocator())
-    ax.xaxis.set_major_locator(plt.MaxNLocator(2))
+    ax.xaxis.set_major_locator(plt.MaxNLocator(3))
     ax.xaxis.set_major_formatter(mdates.DateFormatter(DATETIME_FORMAT))
     plt.xticks(rotation=70)
     plt.show()
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    do_plot(filename)
+
 
