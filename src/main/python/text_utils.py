17f78b61f66fca6fd63b2118495d801ed80a86c1
PhillipHenry <PhillHenry@gmail.com>
2023-07-21 13:44:11 +0100

    Removed arbitrary count

--- src/main/python/text_utils.py
+++ src/main/python/text_utils.py
@@ -138,9 +138,9 @@ def kullback_liebler(docs: [str], ignore_words: [str], ps: dict, qs: dict) -> di
 
 def highest_entropy_words(docs: [str], char_freq: dict, shingles: set, penalty: float) -> [str]:
     word_score = sorted_word_average_entropy(docs, char_freq, shingles, penalty)
-    # max_score = word_score[-1][1]
-    # top = [w for w, s in word_score if s > max_score / 3]
-    top = [w for w, _ in word_score][-18:]  # TODO this is somewhat arbitrary...
+    max_score = word_score[-1][1]
+    top = [w for w, s in word_score if s > max_score / 2]
+    # top = [w for w, _ in word_score][-18:]  # TODO this is somewhat arbitrary...
     return top
 
 
